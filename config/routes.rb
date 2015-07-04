8432c3f505ea2048872ed829323283be9ed00493
Riqwan Thamir <rmthamir@gmail.com>
2015-07-04 22:56:02 +0530

    minor change

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'idea/new'
+
+  get 'idea/create'
+
+  get 'idea/update'
+
+  get 'idea/destroy'
+
   root 'pages#home'
 end
--- db/migrate/20150704171029_create_ideas.rb
+++ db/migrate/20150704171029_create_ideas.rb
@@ -3,7 +3,7 @@ class CreateIdeas < ActiveRecord::Migration
     create_table :ideas do |t|
     	t.references	:user, index: true
     	t.string			:title, index: true
-    	t.string			:description, index: true
+    	t.string			:description
 
       t.timestamps null: false
     end
