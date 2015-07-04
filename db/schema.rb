64be0f7f49f9c799ba4c37c11df3d1d5c5e5f0c4
Nitish Sharma <sharma1nitish@gmail.com>
2015-07-05 01:59:42 +0530

    Integrated Twitter OAuth

--- db/schema.rb
+++ db/schema.rb
@@ -37,6 +37,10 @@ ActiveRecord::Schema.define(version: 20150703034956) do
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
     t.string   "name"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
