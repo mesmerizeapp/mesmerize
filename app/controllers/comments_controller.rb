433e5cb45a05353cfa85842aadc3424aa3dde5ba
Riqwan Thamir <riqwan@keepworks.com>
2015-07-29 12:17:01 +0530

    finish comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,9 @@ class CommentsController < ApplicationController
   end
 
   def create
-    Idea.find(params[:id]).comments.create(comment_params)
+    comment = Idea.find(params[:id]).comments.new(comment_params)
+    comment.user_id = current_user.id
+    comment.save
     redirect_to idea_path(id: params[:id])
   end
 
