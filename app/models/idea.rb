35bfb1b87ef2c0992f612daebffcacd4e1891bbd
Riqwan Thamir <rmthamir@gmail.com>
2015-08-01 16:07:20 +0530

    finish banners for each layout

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,7 +1,7 @@
 class Idea < ActiveRecord::Base
   belongs_to :user
-  has_many  :comments, as: :commentable
-  has_one :description
+  has_many  :comments, as: :commentable, dependent: :destroy
+  has_one :description, dependent: :destroy
 
   after_create :create_description
 end
