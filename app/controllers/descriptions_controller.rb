16c0e16542a34ca2a9e3c962fd29f1ed70f49d71
Riqwan <riqwan@keepworks.com>
2015-08-01 13:10:14 +0530

    add description model, get comments working and update paths

---
+++ app/controllers/descriptions_controller.rb
@@ -0,0 +1,33 @@
+class DescriptionsController < ApplicationController
+  before_action :set_idea
+  before_action :set_description
+
+  layout 'ideas'
+
+  def show
+    @description = @idea.description
+  end
+
+  def edit
+  end
+
+  def update
+    @description.update_attribute(:content, description_params[:content])
+
+    redirect_to idea_description_path(username: params[:username], idea_id: @idea.id)
+  end
+
+  private
+
+  def description_params
+    params.require(:description).permit(:content)
+  end
+
+  def set_idea
+    @idea = Idea.find(params[:idea_id])
+  end
+
+  def set_description
+    @description = @idea.description
+  end
+end
--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,4 +1,7 @@
 class Idea < ActiveRecord::Base
   belongs_to :user
   has_many  :comments, as: :commentable
+  has_one :description
+
+  after_create :create_description
 end
