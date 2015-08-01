8125794e7ddff0e830c23ae7696acbe18261be73
Riqwan Thamir <rmthamir@gmail.com>
2015-08-01 16:48:50 +0530

    remove temp buttons, fix profile routes

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,7 +1,7 @@
 class ProfilesController < ApplicationController
   skip_before_action :authenticate_user!
 
-  def index
+  def show
     @user = User.find_by(username: params[:username])
     @ideas = @user.ideas
   end
