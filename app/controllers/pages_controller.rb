62a05d5a4c99353e9c0cc09fe4d0474648190ad7
Nitish Sharma <sharma1nitish@gmail.com>
2015-08-01 22:10:54 +0530

    Added email update step after sign up

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!
+  skip_before_action :ensure_signup_complete
 
   def home
   end
--- config/routes.rb
+++ config/routes.rb
@@ -4,16 +4,12 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }, skip: [:sessions, :registrations]
 
   as :user do
-    get '/login' => 'devise/sessions#new', as: :new_user_session
-    post '/login' => 'devise/sessions#create', as: :user_session
-    delete '/logout' => 'devise/sessions#destroy', as: :destroy_user_session
-
-    get '/users/cancel' => 'devise/registrations#cancel', as: :cancel_user_registration
-    post '/users' => 'devise/registrations#create', as: :user_registration
-    get '/signup' => 'devise/registrations#new', as: :new_user_registration
-    get '/users/edit' => 'devise/registrations#edit', as: :edit_user_registration
-    match '/users' => 'devise/registrations#update', via: [:patch, :put]
+    post '/login' => 'users/sessions#create', as: :user_session
+    delete '/logout' => 'users/sessions#destroy', as: :destroy_user_session
   end
+    match 'users/finish_signup' => 'users#finish_signup', via: [:get, :patch], as: :finish_signup
+
+
 
   scope ':username' do
     get '', to: 'profiles#show', as: :profile
