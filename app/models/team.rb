f4b8cf5ccb07d291beca99712a69d9a454e24d44
Nitish Sharma <nitish.sharma@keepworks.com>
2015-08-02 22:59:07 +0530

    Added membership mechanism

--- app/models/team.rb
+++ app/models/team.rb
@@ -4,4 +4,12 @@ class Team < ActiveRecord::Base
   has_many :ideas
 
   validates :name, presence: true, uniqueness: true, format: { with: /\A[a-z0-9_-]+\z/ }
+
+  def admins
+    self.memberships.admin.map(&:user)
+  end
+
+  def remove_member(user)
+    self.memberships.find_by(user: user).destroy
+  end
 end
